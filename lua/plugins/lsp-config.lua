88bd1237df01e52cd61bce4f1550c85cb21d97d4
Vincent van Noord <vannoord.vincent@gmail.com>
2025-07-10 09:08:44 +0200

    Initial commit

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,89 +1,106 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local conf = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
+		config = function()
+			local conf = require("lspconfig")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca>", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-      conf.lua_ls.setup({
-        capabilities = capabilities,
-        cmd = { "lua-language-server" }, -- provided by nix
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
+			conf.lua_ls.setup({
+				capabilities = capabilities,
+				cmd = { "lua-language-server" }, -- provided by nix
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-      conf.ts_ls.setup({
-        capabilities = capabilities,
-        cmd = { "typescript-language-server", "--stdio" },
-        settings = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      })
+			conf.ts_ls.setup({
+				capabilities = capabilities,
+				cmd = { "typescript-language-server", "--stdio" },
+				filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+				settings = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			})
 
-      conf.intelephense.setup({
-        capabilities = capabilities,
-        cmd = { "intelephense", "--stdio" },
-        filetypes = { "php" },
-        settings = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-        },
-      })
+			conf.intelephense.setup({
+				capabilities = capabilities,
+				cmd = { "intelephense", "--stdio" },
+				filetypes = { "php" },
+				settings = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			})
 
-      conf.html.setup({
-        capabilities = capabilities,
-        cmd = { "vscode-html-language-server", "--stdio" },
-        filetypes = { "html" },
-      })
+			conf.html.setup({
+				capabilities = capabilities,
+				cmd = { "vscode-html-language-server", "--stdio" },
+				filetypes = { "html" },
+			})
 
-      conf.eslint.setup({
-        capabilities = capabilities,
-        cmd = { "vscode-eslint-language-server", "--stdio" },
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-      })
+			conf.eslint.setup({
+				capabilities = capabilities,
+				cmd = { "vscode-eslint-language-server", "--stdio" },
+				filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+			})
 
-      conf.emmet_ls.setup({
-        capabilities = capabilities,
-        cmd = { "emmet-ls", "--stdio" }, -- from nodePackages.emmet-ls
-        filetypes = {
-          "html",
-          "css",
-          "scss",
-          "javascriptreact",
-          "typescriptreact",
-        },
-        init_options = {
-          html = {
-            options = {
-              ["bem.enabled"] = true,
-            },
-          },
-        },
+			conf.emmet_ls.setup({
+				capabilities = capabilities,
+				cmd = { "emmet-ls", "--stdio" }, -- from nodePackages.emmet-ls
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascriptreact",
+					"typescriptreact",
+				},
+				init_options = {
+					html = {
+						options = {
+							["bem.enabled"] = true,
+						},
+					},
+				},
 
-        conf.tailwindcss.setup({
-          capabilities = capabilities,
-          cmd = { "tailwindcss-language-server", "--stdio" },
-          filetypes = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" }, -- Add more as needed
-          init_options = {
-            userLanguages = {
-              eelixir = "html", -- example mappings, remove if not needed
-              eruby = "html",
-            },
-          },
-        }),
-      })
-    end,
-  },
+				conf.tailwindcss.setup({
+					capabilities = capabilities,
+					cmd = { "tailwindcss-language-server", "--stdio" },
+					filetypes = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte" }, -- Add more as needed
+					init_options = {
+						userLanguages = {
+							eelixir = "html", -- example mappings, remove if not needed
+							eruby = "html",
+						},
+					},
+				}),
+			})
+
+			conf.gopls.setup({
+				capabilities = capabilities,
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
+		end,
+	},
 }
--- lua/vim.lua
+++ lua/vim.lua
@@ -3,8 +3,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.opt.relativenumber = true
+vim.opt.number = true
+
 vim.g.mapleader = " "
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
 
 vim.diagnostic.config({
   virtual_text = true,
@@ -12,3 +15,12 @@ vim.diagnostic.config({
   update_in_insert = false,
   underline = true,
 })
+
+vim.keymap.set("n", "<leader>e", function()
+  vim.diagnostic.open_float(nil, {
+    focus = true,
+    border = "rounded",
+    max_width = 80,
+    scope = "cursor",
+  })
+end, { desc = "Show diagnostic message" })
