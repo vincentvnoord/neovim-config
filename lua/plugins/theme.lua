98dd51c17db95feadb4350ce559d4a3e13fec9ec
Vincent van Noord <vannoord.vincent@gmail.com>
2026-01-10 20:58:12 +0100

    Theme and SuperMaven

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,24 +1,27 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({ flavour = "mocha" })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    priority = 0,
-    config = function()
-      require("gruvbox").setup({
-        undercurl = true,
-        underline = true,
-        bold = true,
-      })
-      -- vim.cmd.colorscheme("gruvbox")
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 0,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				undercurl = true,
+				underline = true,
+				bold = true,
+			})
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 }
